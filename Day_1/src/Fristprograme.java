99dbd10219adbaa34826b638b235a421894f71b6
suyas <suyas@Suyash_Biradar>
2024-10-16 15:45:18 +0530

    day 1 programs

---
+++ Day_1/src/Fristprograme.java
@@ -0,0 +1,10 @@
+
+public class Fristprograme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Frist Program"); 
+
+	}
+
+}
---
+++ Day_1/src/com/tnsif/basicprogram/fristprogram.java
@@ -0,0 +1,11 @@
+package com.tnsif.basicprogram;
+
+public class fristprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("frist program");
+
+	}
+
+}
