33050a9c98810f42f251340c84554204b8bf9cc3
suyas <suyas@Suyash_Biradar>
2024-12-22 22:01:03 +0530

    Polymorphism

---
+++ Day_7/src/com/tnsif/polymorphism/overriding/Shape.java
@@ -0,0 +1,16 @@
+package com.tnsif.polymorphism.overriding;
+
+public class Shape {
+
+	// member function
+		public void draw()
+		{
+			System.out.println("Drawing a generic shape");
+		}
+		
+		
+		public void erase()
+		{
+			System.out.println("Erasing a generic shape");
+		}
+}
---
+++ Day_7/src/com/tnsif/polymorphism/overriding/Square.java
@@ -0,0 +1,31 @@
+package com.tnsif.polymorphism.overriding;
+
+public class Square extends Shape {
+	
+private double side;
+	
+	public Square(double side)
+	{
+		this.side=side;
+	}
+	
+	
+	// Override draw method for square
+	@Override
+	public void draw()
+	{
+		System.out.println("Drawing a square with side "+ side);
+	}
+	
+	
+	// Override erase method for square
+	
+	@Override
+	public void erase()
+	{
+		System.out.println("erasing a square with side : "+ side);
+	}
+	
+	
+
+}
