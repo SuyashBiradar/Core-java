707dc68fae0efbae1be38384bd13b5d50cd48d74
suyas <suyas@Suyash_Biradar>
2025-01-17 17:06:35 +0530

    day 10

---
+++ Day_10/src/com/tnsif/day10/arrayspractice/JaggedArrayDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.day10.arrayspractice;
+
+public class JaggedArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int d[][]= {{10, 20, 30}, {40}, {21, 67}};
+		
+		System.out.println("Total Rows in Array d : "+ d.length);
+		
+		MLArray.printArray(d);
+
+
+	}
+
+}
---
+++ Day_10/src/com/tnsif/day10/arrayspractice/MultiDimArrayDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.day10.arrayspractice;
+
+public class MultiDimArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int c[][] = {{12, 45, 23}, {10, 30, 4}, {2, 3, 78}, {1, 2, 3,}}; // new int[3][3]
+		
+		System.out.println("No of Rows in C array : "+ c.length);
+		
+		MLArray.printArray(c);
+		
+	}
+
+}
